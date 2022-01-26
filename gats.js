f0652a625f70e665616e6025ee80332d18974c1d
sharedrory <85806407+sharedrory@users.noreply.github.com>
2022-01-26 12:36:36 -0500

    Update gats.js

--- gats.js
+++ gats.js
@@ -1,5 +1,32 @@
 const gats = {};
 
+gats.CAPTCHA_SITEKEY = '6LenZt4ZAAAAAF-2nPKzH9111gkjBlaJCEp8UsQV';
+
+gats.Weapon = {
+  Pistol: 0,
+  SMG: 1,
+  Shotgun: 2,
+  Assault: 3,
+  Sniper: 4,
+  LMG: 5,
+};
+
+gats.Color = {
+  Red: 0,
+  Orange: 1,
+  Yellow: 2,
+  Green: 3,
+  Blue: 4,
+  Purple: 5,
+};
+
+gats.Armor = {
+  None: 0,
+  Light: 1,
+  Medium: 2,
+  Heavy: 3,
+};
+
 gats.parsePosition = (data) => {
   return {
     x: data.x / 10,
